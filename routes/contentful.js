3451d6fc878a0c6d81d6f02981aed67c84a6129e
tomnatt <github@tomnatt.com>
2014-11-14 16:25:44 +0000

    added some more retrieval code

--- routes/contentful.js
+++ routes/contentful.js
@@ -15,10 +15,38 @@ router.get('/', function(req, res) {
   client.entries({ 'content_type': config.get("contentful.content_type.leadership_profile_id") }, function(err, entries) {
     if (err) { console.log(err); return; }
 
+    // console.log(entries[0].sys.contentType.sys.id);
+    console.log("Profiles:");
     entries.forEach(function(entry) {
       console.log(entry.fields.title);
     })
+  });
 
+  client.entries({ 'content_type': config.get("contentful.content_type.corporate_information_id") }, function(err, entries) {
+    if (err) { console.log(err); return; }
+
+    console.log("Corporate infomation:");
+    entries.forEach(function(entry) {
+      console.log(entry.fields.title);
+    })
+  });
+
+  client.entries({ 'content_type': config.get("contentful.content_type.team_overview_id") }, function(err, entries) {
+    if (err) { console.log(err); return; }
+
+    console.log("Teams:");
+    entries.forEach(function(entry) {
+      console.log(entry.fields.title);
+    })
+  });
+
+  client.entries({ 'content_type': config.get("contentful.content_type.location_id") }, function(err, entries) {
+    if (err) { console.log(err); return; }
+
+    console.log("Locations:");
+    entries.forEach(function(entry) {
+      console.log(entry.fields.title);
+    })
   });
 
   res.send('respond with a resource');
